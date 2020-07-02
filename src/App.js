f8f8088e5f993d6a2094a3572fca346d2631a66d
Samikshya <samikshyaaryal5@gmail.com>
2020-07-02 14:00:49 +0300

    using hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "20ffc04d";
+  const APP_KEY = "e6c7f739e935a020ab7892d9d9afc4ae";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    console.log('effect has been run');
+  }, [counter]); //only when counter changes the useEEffet runs. 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="search-form">
+    <input className="search-bar" type="text" />
+    <button className="sarch-button" type="submit">Search</button>
+    </form>
+    <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+const APP_ID = "20ffc04d";
+const APP_KEY = "e6c7f739e935a020ab7892d9d9afc4ae";
