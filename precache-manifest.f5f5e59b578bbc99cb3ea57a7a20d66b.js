dd179e9cf450bf03b359e271ffbd33c2713a5f29
Samikshya <samikshyaaryal5@gmail.com>
2020-07-03 13:39:06 +0300

    Updates

---
+++ precache-manifest.f5f5e59b578bbc99cb3ea57a7a20d66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cb11f6e7a5cd57c66ccf5acc091877",
+    "url": "/Recipe-search/index.html"
+  },
+  {
+    "revision": "bee9f67a82faaf5cb487",
+    "url": "/Recipe-search/static/css/main.98bee765.chunk.css"
+  },
+  {
+    "revision": "44201ec6102984692f03",
+    "url": "/Recipe-search/static/js/2.ff07b7d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-search/static/js/2.ff07b7d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee9f67a82faaf5cb487",
+    "url": "/Recipe-search/static/js/main.bb0288a7.chunk.js"
+  },
+  {
+    "revision": "5db04575375c05ab551e",
+    "url": "/Recipe-search/static/js/runtime-main.d590dee5.js"
+  }
+]);
